e1ac70cb136fe728595c02be000f8bb0b96dded6
israeel <israeel.abid@gmail.com>
2022-09-05 11:13:27 +0430

    first commit

---
+++ my first javascript.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+function myfunc(){
+
+
+    document.getElementById("demo").innerHTML="something somthing"
+    alert("hi this is header")
+
+
+}
+ document.write(5 +10);
+
+
+window.alert("hi iam window alert");
+var x = 5;
+var y = 6;
+var z = x + y;
+
+console.log(z)
+
